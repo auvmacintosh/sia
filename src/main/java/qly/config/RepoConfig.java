6421925f474b8c166fa316dd2c42c68b8af8233f
auvmacintosh <120029790@qq.com>
2018-08-12 18:06:24 +0800

    jndi finished

--- src/main/java/qly/config/RepoConfig.java
+++ src/main/java/qly/config/RepoConfig.java
@@ -2,17 +2,17 @@ package qly.config;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.naming.NamingException;
 import javax.sql.DataSource;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import qly.data.QlyUserMapper;
 
 @Configuration
@@ -20,26 +20,43 @@ import qly.data.QlyUserMapper;
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
-
+@PropertySource({"classpath:persistence-jndi.properties"})
 public class RepoConfig {
 
-    public DataSource getDataSource() {
-        DataSource dataSource = new MysqlDataSource();
-        ((MysqlDataSource) dataSource).setUrl("jdbc:mysql://localhost:3306/wangbodb?useSSL=false");
-        ((MysqlDataSource) dataSource).setUser("root");
-        ((MysqlDataSource) dataSource).setPassword("Wb1302!!!");
-        return dataSource;
+//    public DataSource dataSource2() {
+//        final JndiDataSourceLookup dsLookup = new JndiDataSourceLookup();
+//        dsLookup.setResourceRef(true);
+//        DataSource dataSource = dsLookup.getDataSource("jdbc/wbqly");
+//        return dataSource;
+//    }
+
+//    public DataSource dataSource1() {
+//        JndiObjectFactoryBean jndiObjectFactoryBean =
+//                new JndiObjectFactoryBean();
+//        jndiObjectFactoryBean.setJndiName("jdbc/wbqly");
+//        jndiObjectFactoryBean.setResourceRef(true);
+//        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+//        return (DataSource) jndiObjectFactoryBean.getObject();
+//    }
+
+    @Autowired
+    private Environment env;
+
+    public DataSource dataSource() throws NamingException {
+//        return (DataSource) new JndiTemplate().lookup(env.getProperty("jdbc.url"));
+        return (DataSource) new JndiTemplate().lookup("java:comp/env/db");
     }
 
     @Bean
-    public QlyUserMapper getQlyUserMapper() throws Exception{
+    public QlyUserMapper getQlyUserMapper() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(getDataSource());
+        sqlSessionFactoryBean.setDataSource(dataSource());
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
         sqlSessionFactory.getConfiguration().addMapper(QlyUserMapper.class);
         SqlSession sqlSession = sqlSessionFactory.openSession();
         QlyUserMapper qlyUserMapper = sqlSession.getMapper(QlyUserMapper.class);
         return qlyUserMapper;
     }
+
 
 }
--- src/main/java/qly/config/WebConfig.java
+++ src/main/java/qly/config/WebConfig.java
@@ -1,13 +1,23 @@
 package qly.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+//import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.annotation.Resource;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -28,4 +38,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
 }
